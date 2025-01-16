7f2b6b6b4bd47684e885a415d6c5df1775976ca6
AsumVictor <iamasum369@gmail.com>
2025-01-16 13:26:12 +0000

    Added upload: err1

--- upload_service/controller/upload.controller.js
+++ upload_service/controller/upload.controller.js
@@ -22,23 +22,102 @@ const initialized_upload = catchAsyncError(async (req, res, next) => {
       Bucket: process.env.AWS_BUCKET_NAME,
       ContentType: "video/mp4",
     };
-    console.log("multipartparams---- ");
     const multipart = await s3_upload.createMultipartUpload(params).promise();
-    console.log("multipartparams---- ", multipart);
+    // console.log("multipartparams---- ", multipart);
     const upload_id = multipart.UploadId;
 
     res.status(200).json({ upload_id });
   } catch (error) {
+    console.error("Error initializing upload:", err);
     return next(new ResponseError(error.message, 400));
   }
 });
 
-const upload_chunk = catchAsyncError(async (req, res) => {
-  res.json("Work");
+// Here we upload the actual code chunks
+const upload_chunk = catchAsyncError(async (req, res, next) => {
+  try {
+    const { filename, chunkIndex, uploadId } = req.body;
+
+    if (!filename || !chunkIndex || !uploadId) {
+      return next(new ResponseError("importance fileds are required", 400));
+    }
+
+    const s3_upload = new aws.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECURITY_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+    });
+
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: filename,
+      UploadId: uploadId,
+      PartNumber: parseInt(chunkIndex) + 1,
+      Body: req.file.buffer,
+    };
+
+    const result = await s3_upload.uploadPart(params).promise();
+    console.log("data------- ", result);
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.error("Error uploading chunk:", err);
+    return next(new ResponseError(error.message, 400));
+  }
 });
 
-const complete_upload = catchAsyncError(async (req, res) => {
-  res.json("Work");
+const complete_upload = catchAsyncError(async (req, res, next) => {
+  try {
+    console.log("Completing Upload");
+    const { filename, totalChunks, uploadId } = req.body;
+
+    const uploadedParts = [];
+
+    // Building all uploadedParts array from request body
+    for (let i = 0; i < totalChunks; i++) {
+      uploadedParts.push({ PartNumber: i + 1, ETag: req.body[`part${i + 1}`] });
+    }
+
+    const s3_upload = new aws.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECURITY_ACCESS_KEY,
+      region: process.env.AWS_REGION,
+    });
+
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: filename,
+      UploadId: uploadId,
+    };
+
+    // Listing all parts using promise
+    const data = await s3_upload.listParts(params).promise();
+
+    const parts = data.Parts.map((part) => ({
+      ETag: part.ETag,
+      PartNumber: part.PartNumber,
+    }));
+
+    params.MultipartUpload = {
+      Parts: parts,
+    };
+
+    // Completing multipart upload using promise
+    const uploadResult = await s3_upload
+      .completeMultipartUpload(params)
+      .promise();
+
+    console.log("data----- ", uploadResult);
+
+    // await addVideoDetailsToDB(title, description , author, uploadResult.Location);
+    // pushVideoForEncodingToKafka(title, uploadResult.Location);
+    return res
+      .status(201)
+      .json({ uploadResult, message: "Uploaded successfully!!!" });
+
+  } catch (error) {
+    console.log("Error handling complete", error);
+    return next(new ResponseError(error.message, 400));
+  }
 });
 
 module.exports = {
