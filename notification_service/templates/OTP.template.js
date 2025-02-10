e5580f76a68b060ae78f5ec4af55ed4400517784
Victor  Asum <victor.asum@ashesi.edu.gh>
2025-02-10 00:06:29 +0000

    Added(core): Initialized Notification servcies

---
+++ notification_service/templates/OTP.template.js
@@ -0,0 +1,7 @@
+const OTP_template = (user_name, email, link, code) => {
+  return `
+    some HTML syntax here
+    `;
+};
+
+export default OTP_template;
---
+++ notification_service/templates/index.js
@@ -0,0 +1,7 @@
+import * as OTP from "./OTP.template.js";
+import * as CommentReply from "./OTP.template.js";
+
+export {
+    OTP,
+    CommentReply
+}
