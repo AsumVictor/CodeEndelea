0c1c64acb1266554164b934c1c87974bfb68cb27
AsumVictor <iamasum369@gmail.com>
2025-02-21 16:59:48 +0000

    Added(Core\:) add feature

--- authentification_service/model/user.model.js
+++ authentification_service/model/user.model.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const videoSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
-    user_name: {
+    username: {
       required: [true, "Username is required!"],
       type: String,
       unique: [true, "Username is already taken"],
@@ -18,8 +19,25 @@ const videoSchema = new mongoose.Schema(
     password: {
       type: String,
       required: [true, "Password is required"],
-      
-    }
+    },
+    access: {
+      type: Number,
+      // Access Level admin: 1111, student:1121, tutor: 1122
+      enum: [1111, 1121, 1122],
+      required: [true, "User access control limit is required"],
+      default: 1121,
+      // Default access level is student
+    },
+    reset_password_token: {
+      type: String,
+      default: "",
+    },
+    reset_password_time: {
+      type: Date,
+    },
+    /**
+     * @
+     */
   },
   {
     timestamps: true,
@@ -27,15 +45,36 @@ const videoSchema = new mongoose.Schema(
   }
 );
 
-/*
+// Check
+//  Hash password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
 
-- A video have title, description, length, exercise timestamps -> [],
-- screen hls url, screen url, camera hls url, camera url
-- reactions of students
-- 
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
+// jwt token
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
 
+// compare password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-*/
+// hash resetToken
+userSchema.methods.hashToken = async function (token) {
+  return await bcrypt.hash(token, 10);
+};
 
-export default mongoose.model("video-metadata-v1", videoSchema);
+// compare token
+userSchema.methods.compareToken = async function (token) {
+  return await bcrypt.compare(token, this.resetPasswordToken);
+};
+
+export default mongoose.model("user-v1", userSchema);
